98c75663e173706ef78cb8cd5a358b90c12337d3
Phil Aquilina <philaquilina@gmail.com>
2012-07-26 12:16:44 -0700

    Add annotations to all models.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id               :integer          not null, primary key
+#  content          :string(255)      not null
+#  user_id          :integer          not null
+#  commentable_id   :integer          not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  commentable_type :string(255)
+#
+
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :commentable, :polymorphic => true
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: votes
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer          not null
+#  link_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Vote < ActiveRecord::Base
   attr_accessible :link_id
 
