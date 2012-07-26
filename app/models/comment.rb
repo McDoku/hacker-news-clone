c64b37d6bc4ed040a0bc099fa0faa47e282893a2
Michael Kaiser-Nyman <michaelrkn@gmail.com>
2012-07-25 17:03:53 -0700

    models for comment polymorphism

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,12 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
-  belongs_to :link
+  belongs_to :link, :polymorphic => true
+  belongs_to :comment, :polymorphic => true
+  has_many :comments, :as => :parent
 
   attr_accessible :content, :link_id
 
   validates :content, :presence => true
   validates :user_id, :presence => true
-  validates :link_id, :presence => true
+  validates :parent_id, :presence => true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120724212134) do
+ActiveRecord::Schema.define(:version => 20120725224323) do
 
   create_table "comments", :force => true do |t|
-    t.string   "content",    :null => false
-    t.integer  "user_id",    :null => false
-    t.integer  "link_id",    :null => false
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string   "content",     :null => false
+    t.integer  "user_id",     :null => false
+    t.integer  "parent_id",   :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+    t.string   "parent_type"
   end
 
   create_table "links", :force => true do |t|
