b5ae88a1236ab6a1af50f8306af36b02a0f377e7
Michael Kaiser-Nyman <michaelrkn@gmail.com>
2012-07-26 17:10:42 -0700

    allow downvotes

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,13 +1,23 @@
+# class DirectionValidator < ActiveModel::Validator
+#   def validate(record, attribute, value)
+#     unless value == 1 || value == -1
+#       record.errors[attribute] << (options[:message]) || "is not plus or minus 1"
+#     end
+#   end
+# end
+
 class Vote < ActiveRecord::Base
-  attr_accessible :votable
+  attr_accessible :votable, :direction
 
   belongs_to :user
   belongs_to :link
   belongs_to :votable, :polymorphic => true
 
   validates_uniqueness_of :user_id, :scope => [:votable_id, :votable_type]
+  # validates_numericality_of :direction, :if => Proc.new {|v| v.direction.abs == 1}
 
   validates :user,         :presence => true
+  validates :direction,    :presence => true
   validates :votable,      :presence => true
   validates :votable_type, :presence => true
 end
