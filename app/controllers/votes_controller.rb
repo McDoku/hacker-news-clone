d7e56bcf668add757800a4206a4f69870a5123ae
michael <michaelrkn@gmail.com>
2012-07-28 11:01:51 -0700

    moved logic to load votable into a method to match format of load_commentable

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,13 +1,19 @@
 class VotesController < ApplicationController
+  before_filter :load_votable
+  
   def create
-    if params[:link_id]
-      votable = Link.find(params[:link_id])
-    else
-      votable = Comment.find(params[:comment_id])
-    end
-
-    current_user.votes.create(:votable => votable, :direction => params[:direction])
+    current_user.votes.create(:votable => @votable, :direction => params[:direction])
     flash[:success] = "Thanks for voting."
     redirect_to :back
+  end
+  
+  private
+  
+  def load_votable
+    if params[:link_id]
+      @votable = Link.find(params[:link_id])
+    else
+      @votable = Comment.find(params[:comment_id])
+    end
   end
 end
